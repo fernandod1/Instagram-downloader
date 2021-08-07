9f45ee4608b0e590d99ec5fa8dbd0f4bf5fe27ba
Chris <atnl@protonmail.com>
2021-08-07 14:10:10 -0400

    Update instagram_downloader.py

--- instagram_downloader.py
+++ instagram_downloader.py
@@ -11,7 +11,7 @@ import urllib.request
 import os
 import json
 
-INSTAGRAM_USERNAME = "ladygaga"
+INSTAGRAM_USERNAME = " "
 
 # ------------------------------Do not modify under this line--------------------------------------- #
 
