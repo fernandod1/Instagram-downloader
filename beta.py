2911854802395643573edbe666cbd6cfe375342c
Chris <atnl@protonmail.com>
2021-08-13 14:51:37 -0400

    Update beta.py

--- beta.py
+++ beta.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-# Much of this code was inspired by James Kettle's HTTP Request Smuggling Python tool/code.
+# Much of this code was inspired by James Kettle's HTTP Request Smuggling Python tool/code
 import argparse
 import re
+import requests
 import time
 import sys
 import os
@@ -11,6 +12,7 @@ import random
 import string
 import importlib
 import hashlib
+#import username
 from lib import Url
 from copy import deepcopy
 from time import sleep
@@ -36,15 +38,15 @@ from lib.colorama import Fore, Style
 # For OSINT purposes, Add a a Metadata exfiltration feature, (linux cmd line tools out there for that and Python scripts) could make them modules and import them.
 # Is it possible to proxy individual requests/downloads through diffent servers to prevent IP ban?
 # Offer the ability to download more than just one Instagrammer at a time. (need proxies?)
-# ?? import threading module & create def queueRequests(): 
+# ?? import threading module & create def queueRequests():
 # Or, use asyncio or a different concurrency option? could conflict with API calls and result in being blocked
 # How to succienctly call the classe(s) and functions to run the program ??
 # ?? Any sepearate modules needed for importation?
 # Probably need to up the sleep() function to 1 second time.sleep(1.00)  # pause to avoid ban
 # Ultimately how to run the program? ie: Calling the classes, functions etc, a simple oneliner? or multiple lines calling each function sepearately?
-# Fix indentation and clean up the code & remember.. 
+# Fix indentation and clean up the code & remember..
 
-# -------The ZEN of PYTHON-------
+# -------The ZEN of PYTHON-------#
 # Complex is better than complicated.
 # Flat is better than nested.
 # Sparse is better than dense.
@@ -74,93 +76,116 @@ smhost = "https://instagram.com"
 # This indentation is..Ughhh, fix it Chris.
 # Is the  (): needed on this class creation?
 # Can you do it the basic way using just the : ??
-class Instagram_Downloader(): 
-		'''Some of James Kettle's smuggler.py attributes for REFERENCE'''
 
-		def __init__(self, smhost, username=""):
-				self._host = smhost  # does this Internal Attribute need the underscore prefix? 
-				self.username = username
-				self.user_id = ""
-				self.jsondata = ""
-				self.apilabel = "graphql"
-				self.hash_timeline = ""
 
-		def run(self):
-			"""receivess the call sm.run() to execute the code. I want to do run this as succiently as possible"""
-			while True:
-				Parser = argparse.ArgumentParser()
+class Instagram_Downloader:
+	def __init__(self, smhost, username=""):
+		self._host = smhost  # Does this attribute need the ._ for data access?
+		self.username = username
+		self.user_id = ""
+		self.jsondata = ""
+		self.apilabel = "graphql"
+		self.hash_timeline = ""
 
-		def create_download_directory(self):
+	def run(self):
+		"""receivess the call sm.run() to execute the code"""
+		while True:
+			Parser = argparse.ArgumentParser()
+
+	def create_download_directory(self):
+		try:
+			os.mkdir(self.username)
+			print("Directory ", self.username, " created.")
+		except FileExistsError:
+			print("Directory ", self.username, " already exists.")
+
+	def get_total_photos(self):
+		return int((self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["count"]))
+
+	def set_user_id(self):
+		self.user_id = int(self.jsondata.json()[self.apilabel]["user"]["id"])
+
+	def get_end_cursor_timeline_media(self):
+		return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
+
+	def write_resume_end_cursor_timeline_media(self):
+		# create file with last page downladed to resume if needed
+		f = open("resume_"+self.username+".txt", "w")
+		f.write(str(self.get_end_cursor_timeline_media()))
+		f.close()
+
+	def read_resume_end_cursor_timeline_media(self):
+		if os.path.isfile("resume_"+self.username+".txt"):
+			with open("resume_"+self.username+".txt") as f:
+				self.hash_timeline = f.readline().strip()
+				print("****************\nResume mode ON\n****************")
+				return True
+		else:
+			print("Not resume pending")
+			return False
+
+	def remove_resume_file(self):
+		if os.path.exists("resume_"+self.username+".txt"):
+			os.remove("resume_"+self.username+".txt")
+
+	def set_apilabel(self, label):
+		self.apilabel = label
+
+	# A lot of this lengthy & hard to read code could be eliminated with aan account login feature.
+	def has_next_page(self):
+		return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
+
+	def get_jsondata_instagram(self):
+		headers = {
+                    "Host": "www.instagram.com", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+		}
+		if self.apilabel == "graphql":
+			self.jsondata = requests.get(
+			    "https://www.instagram.com/" + self.username + "/feed/?__a=1", headers=headers)
+			self._set_user_id()
+		else:
+			self.jsondata = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(
+			    self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D", headers=headers)
+        		if user.has_next_page(): #This error keeps poping up:  if user.has_next_page():  TabError: inconsistent use of tabs and spaces in indentation
+				self.hash_timeline = self.get_end_cursor_timeline_media()
+		if (self.jsondata.status_code == 200):
+			print("----------------\nJson url loaded:\n----------------\nhttps://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" +
+			      str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D")
+		else:
+			print("Error: Incorrect json data url received.")
+		return self.jsondata
+
+	def download_photos(self):
+		i = 0
+		for match in self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"]:
+			time.sleep(1.00)
+			print("Downloading photos:\n" + match["node"]["display_url"])
+			filename = self.username + "/" + self.jasondata.jason(
+			)[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"][i]["node"]["shortcode"] + ".jpg"
 			try:
-				os.mkdir(self.username)
-				print("Directory ", self.username, " created.")
-			except FileExistsError:
-				print("Directory ", self.username, " already exists.")
+				if not os.path.exists(filename):
+					urllib.requests.urlretrieve(match["node"]["display_url"], filename)
+				else:
+					print("Notice: " + filename + " image already downloaded, skipped.")
+			except urllib.error.HTTPError as e:
+				print(str(e.code) + "Unable to download image")
+			i = i + 1
 
-		def get_total_photos(self):
-			return int((self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["count"]))
-
-		def set_user_id(self):
-			self.user_id = int(self.jsondata.json()[self.apilabel]["user"]["id"])
-
-		def get_end_cursor_timeline_media(self):
-			return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
-
-		def write_resume_end_cursor_timeline_media(self):
-			# create file with last page downladed to resume if needed
-			f = open("resume_"+self.username+".txt", "w")
-			f.write(str(self.get_end_cursor_timeline_media()))
-			f.close()
-
-		def read_resume_end_cursor_timeline_media(self):
-			if os.path.isfile("resume_"+self.username+".txt"):
-				with open("resume_"+self.username+".txt") as f:
-					self.hash_timeline = f.readline().strip()
-					print("****************\nResume mode ON\n****************")
-					return True
-			else:
-				print("Not resume pending")
-				return False
-
-		def remove_resume_file(self):
-			if os.path.exists("resume_"+self.username+".txt"):
-				os.remove("resume_"+self.username+".txt")
-
-		def set_apilabel(self, label):
-			self.apilabel = label
-                 
-		# A lot of this lengthy & hard to read code could be eliminated with aan account login feature. 
-		def has_next_page(self):
-			return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
-
-		def get_jsondata_instagram(self):
-			headers = {
-				"Host": "www.instagram.com"
-				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
-				}
-			if self.apilabel == "graphql":
-					self.jsondata = requests.get(
-							"https://www.instagram.com/" + self.username + "/feed/?__a=1", headers=headers)
-					self.hash_timeline = self.get_end_cursor_timeline_media()
-					self.set_user_id()
-				else:   # All this could be easily cleaned up & possibly eliminated by using a dummy account & login, like with the emailbomber.py
-					self.jsondata = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(
-							self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D", headers=headers)
-					if user.has_next_page():
-						self.hash_timeline = self.get_end_cursor_timeline_media()
-					if (self.jsondata.status_code == 200):
-						print("----------------\nJson url loaded:\n----------------\nhttps://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" +
-						      str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D")
+	def download_videos(self):
+		i = 0
+		if 'edge_felix_video_timeline' in self.jasondata.jason()[self.apilabel]["user"]:
+			for match in self.jsondata.json()[self, apilabel]["user"]["edge_felix_video_timeline"]["edges"]:
+				time.sleep(1.00)
+				print("Downloading Video:\n" + match["node"]["video_url"])
+				filename = self.username + "/" self.jsondata.json()[self.apilabel][apilabel]["user"]["edge_felix_video_timeline"]["edges"][i]["node"]["shortcode"] + ".mp4"
+				try:
+					if not os.path.exists(filename):
+						urllib.requests.urlretrieve(match["node"]["video_url"], filename)
 					else:
-						print("Error, Incorrect json data url recieved.")
-					return self.jsondata
-
-		def download_photos(self):
-			pass
-	
-
-		def download_videos(self):
-			pass
+						print("Notice: " + filename + " video already downloaded, skipped.")
+				except urllib.error.HTTPError as e:
+					print(str(e.code) + "Unable to download video.")
+				i = i + 1
 
 
 # --------------------------------- SMUGGLER CODE -------------------------------------#
@@ -219,7 +244,7 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 # Goal: Offer the ability to select more than just one Instagram User
-# Redo-EDIT this menu for that pupose.
+# Redo-EDIT this menue for its puposes.
 Parser = argparse.ArgumentParser()
 Parser.add_argument('-u', '--user', help="INSTAGRAM_USERNAME")
 Parser.add_argument('-x', '--exit_early', action='store_true',
@@ -243,7 +268,7 @@ sm.run()
 
 
 # Original way of calling/executing Instagram Downloader script
-# Keep this, possible need it as code progesses, maybe need to stich it in whith what you have now.
+
 # -----------Main program function calls for Instagram_Downloader -------------------------------------#
 #
 # try:
@@ -269,3 +294,4 @@ sm.run()
 #    print("Notice: script premature finished due to daily limit of number of requests to Instagram API.")
 #    print("Just execute script AGAIN in few hours to continue RESUME of pending images/videos to downloa#
 #
+
