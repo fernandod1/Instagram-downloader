70022e07392c3a08012295e9582fec98c48acc10
Chris <atnl@protonmail.com>
2021-08-19 21:31:05 -0400

    Update ig_downloader_modified.py

--- ig_downloader_modified.py
+++ ig_downloader_modified.py
@@ -19,88 +19,111 @@ from datetime import datetime
 from instagram_downloader import user
 from lib.colorama import Fore, Style
 
-# Doesn't run unless I privide the URL twice as two simmilar but different variables
-host = "https://instagram.com"
-smhost = "https://instagram.com"
-# -------------------------------------------------------------------------------------------------------- #
+# Doesn't run unless I provide the same URL twice as two similar but different variables, (redundant)
+host="https://instagram.com"
+smhost="https://instagram.com"
 
-#  Any modifications to the current attributes needed???
+
+# -------------------------------------------------------------------------------------------------------- #
+# Update: renamed all instances of username to user
+# Any modifications to the current attributes needed???
+# Seems like something is not quite right
 class Instagram_Downloader:
-    def __init__(self, username=""):
-        self.username = username
+
+    def __init__(self, user):
+        self.user = user
         self.user_id = ""
-        self.jsondata = ""
+        self.jasondata = ""
         self.apilabel = "graphql"
         self.hash_timeline = ""
 
-    # This method's not working so well
-    def get_username(self):
-        return self.username
 
      # Main function that's supposed to run the script.
      # As of now, program just runs as an infinite loop, (but the banner is displying)
-     # Needs work on parsing 
-    def run(self):
-        while True:
-            Parser = argparse.ArgumentParser()
+     # Needs work on parsing?
+     # Original main function:
+     # def run(self):
+     #  while True:
+     #      Parser = argparse.ArgumentParser()
 
-    # Not currently creating a directory, except sometimes just an empty ' '	
+     # REWORKING the MAIN function.
+    def run(self):
+        pass
+
+    # This method's not working so well
+    #def get_user(self):
+    #    return self.username
+
+    # Augmenting this method too
+    def get_user(self, user):
+        return self.user
+
+    # No.
+    name=property(get_user)
+
+    # Creates an empty ' '
     def create_download_directory(self):
         try:
-            os.mkdir(self.username)
-            print("Directory ", self.username, " created.")
+            user = self.user
+            os.mkdir(self.user.new_dir(user))
+            print("Directory ", self.user.new_dir, " created.")
         except FileExistsError:
-            print("Directory ", self.username, " already exists.")
+            print("Directory ", self.user.new_dir, " already exists.")
 
     def get_total_photos(self):
         return int((self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["count"]))
 
     def set_user_id(self):
-        self.user_id = int(self.jsondata.json()[self.apilabel]["user"]["id"])
+        self.user_id=int(self.jsondata.json()[self.apilabel]["user"]["id"])
 
     def get_end_cursor_timeline_media(self):
         return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
 
     def write_resume_end_cursor_timeline_media(self):
-        f = open("resume_"+self.username+".txt", "w")   # create file with last page downladed to resume if needed
+        f=open("resume_" + self.user + ".txt", "w")  # create file with last page downladed to resume if needed
         f.write(str(self.get_end_cursor_timeline_media()))
         f.close()
 
     def read_resume_end_cursor_timeline_media(self):
-        if os.path.isfile("resume_"+self.username+".txt"):
-            with open("resume_"+self.username+".txt") as f:
-                self.hash_timeline = f.readline().strip()
-                print ("****************\nResume mode ON\n****************")
+        if os.path.isfile("resume_" + self.user + ".txt"):
+            with open("resume_" + self.user + ".txt") as f:
+                self.hash_timeline=f.readline().strip()
+                print("****************\nResume mode ON\n****************")
                 return True
         else:
-            print ("Not resume pending")
+            print("Not resume pending")
             return False
 
     def remove_resume_file(self):
-        if os.path.exists("resume_"+self.username+".txt"):
-            os.remove("resume_"+self.username+".txt")
+        if os.path.exists("resume_" + self.user + ".txt"):
+            os.remove("resume_" + self.user + ".txt")
 
-    def set_apilabel(self,label):
-        self.apilabel = label
+    def set_apilabel(self, label):
+        self.apilabel=label
 
     def has_next_page(self):
         return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
 
     def get_jsondata_instagram(self):
-        headers = {
+        headers={
             "Host": "www.instagram.com",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
         }
-        if self.apilabel=="graphql":
-            self.jsondata = requests.get("https://www.instagram.com/" + self.username + "/feed/?__a=1", headers=headers)
-            self.hash_timeline = self.get_end_cursor_timeline_media()
+        if self.apilabel == "graphql":
+            self.jsondata=requests.get("https://www.instagram.com/" + self.user + "/feed/?__a=1", headers=headers)
+            self.hash_timeline=self.get_end_cursor_timeline_media()
             self.set_user_id()
         else:
-            self.jsondata = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D", headers=headers)
+            self.jsondata=requests.get(
+                "https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(
+                    self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D",
+                headers=headers)
             if user.has_next_page():
-                self.hash_timeline = self.get_end_cursor_timeline_media()
-        if (self.jsondata.status_code==200):
-            print("----------------\nJson url loaded:\n----------------\nhttps://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D")
+                self.hash_timeline=self.get_end_cursor_timeline_media()
+        if (self.jsondata.status_code == 200):
+            print(
+                "----------------\nJson url loaded:\n----------------\nhttps://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(
+                    self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D")
         else:
             print("ERROR: Incorrect json data url recieved.")
         return self.jsondata
@@ -109,13 +132,15 @@ class Instagram_Downloader:
         i = 0
         for match in self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"]:
             time.sleep(0.5)
-            print("Downloading IMAGE:\n" +match["node"]["display_url"])
-            filename = self.username + "/" + self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"][i]["node"]["shortcode"] + ".jpg"
+            print("Downloading IMAGE:\n" + match["node"]["display_url"])
+            filename=self.user + "/" + \
+                     self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"][i]["node"][
+                         "shortcode"] + ".jpg"
             try:
-                if not os.path.exists(filename ):
+                if not os.path.exists(filename):
                     urllib.request.urlretrieve(match["node"]["display_url"], filename)
                 else:
-                    print("Notice: "+ filename+ " image already downloaded, skipped.")
+                    print("Notice: " + filename + " image already downloaded, skipped.")
             except urllib.error.HTTPError as e:
                 print(str(e.code) + " Can't download image")
             i = i + 1
@@ -125,27 +150,29 @@ class Instagram_Downloader:
         if 'edge_felix_video_timeline' in self.jsondata.json()[self.apilabel]["user"]:
             for match in self.jsondata.json()[self.apilabel]["user"]["edge_felix_video_timeline"]["edges"]:
                 time.sleep(0.5)
-                print("Downloading VIDEO:\n" +match["node"]["video_url"])
-                filename = self.username + "/" + self.jsondata.json()[self.apilabel]["user"]["edge_felix_video_timeline"]["edges"][i]["node"]["shortcode"] + ".mp4"
+                print("Downloading VIDEO:\n" + match["node"]["video_url"])
+                filename=self.user + "/" + \
+                         self.jsondata.json()[self.apilabel]["user"]["edge_felix_video_timeline"]["edges"][i]["node"][
+                             "shortcode"] + ".mp4"
                 try:
-                    if not os.path.exists(filename ):
+                    if not os.path.exists(filename):
                         urllib.request.urlretrieve(match["node"]["video_url"], filename)
                     else:
-                        print("Notice: "+ filename+ " video already downloaded, skipped.")
+                        print("Notice: " + filename + " video already downloaded, skipped.")
                 except urllib.error.HTTPError as e:
                     print(str(e.code) + " Can't download video")
                 i = i + 1
 
 
- #---------------------------------Color banner and user input menu -------------------------------------#
+# ---------------------------------Color banner and user input menu -------------------------------------#
 
-# Create menu.py and import it as a module instead of displaaying it here???
+# Create menu.py and import it as a module instead of displaying it here???
 def CF(text):
-	global NOCOLOR
-	if NOCOLOR:
-		ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
-		text = ansi_escape.sub('', text)
-	return text
+    global NOCOLOR
+    if NOCOLOR:
+        ansi_escape=re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+        text=ansi_escape.sub('', text)
+    return text
 
 
 def banner(sm_version):
@@ -173,43 +200,41 @@ def banner(sm_version):
 	print(CF(r"|________/   \_______/  \ ____/   \____/  |_|    \_______________/  /_/         \______________________________/       \_\   "))
 	print(CF(r"                                                                                                                             "))
 	print(CF(r"                                                                                                                             "))
-	print(CF(r"     LinuxUser255                                                                                        				 %s" % (sm_version)))
+	print(CF(r"     LinuxUser255                                                                                        		 %s" % (sm_version)))
 	print(CF(Style.RESET_ALL))
 
 
 def print_info(msg, file_handle=None):
-	ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
-	msg = Style.BRIGHT + Fore.MAGENTA + \
-            "[%s] %s" % (Fore.CYAN + '+' + Fore.MAGENTA, msg) + Style.RESET_ALL
-	plaintext = ansi_escape.sub('', msg)
-	print(CF(msg))
-	if file_handle is not None:
-		file_handle.write(plaintext + "\n")
+    ansi_escape=re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+    msg=Style.BRIGHT + Fore.MAGENTA + \
+        "[%s] %s" % (Fore.CYAN + '+' + Fore.MAGENTA, msg) + Style.RESET_ALL
+    plaintext=ansi_escape.sub('', msg)
+    print(CF(msg))
+    if file_handle is not None:
+        file_handle.write(plaintext + "\n")
+
 
 if __name__ == "__main__":
-	global NOCOLOR
-	if sys.version_info < (3, 0):
-		print("Error: Instagram Downloader requires Python 3.x")
-		sys.exit(1)
+    global NOCOLOR
+    if sys.version_info < (3, 0):
+        print("Error: Instagram Downloader requires Python 3.x")
+        sys.exit(1)
 
 # Goal: Offer the ability to select more than just one Instagram User
-# Something is off when taking the -u argument
-Parser = argparse.ArgumentParser()
-Parser.add_argument('-u', '--user', help="INSTAGRAM_USERNAME")
+# Something's off here, you're not using argparse correctly
+Parser=argparse.ArgumentParser()
+Parser.add_argument('-u', type=str, help='username')
 Parser.add_argument('-x', '--exit_early', action='store_true', help="Exit scan on first finding")
 Parser.add_argument('--no-color', action='store_true', help="Suppress color codes")
-#Parser.add_argument('-c', '--configfile', default="default.py",help="Filepath to the configuration file of payloads")
-Args = Parser.parse_args()  # returns data from the options specified (ech
+Args=Parser.parse_args()  # returns data from the options specified (ech
 
-
-NOCOLOR = Args.no_color
+NOCOLOR=Args.no_color
 if os.name == 'nt':
-	NOCOLOR = True
+    NOCOLOR=True
 
-Version = "v2.0"
+Version="v2.0"
 banner(Version)
 
-# Call it
-sm = Instagram_Downloader(username="") #append .run() instead of calling sm.run() below ?? That kinda works.  
+# Run it
+sm=Instagram_Downloader(user)
 sm.run()
-
